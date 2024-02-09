e3f7fa4fa58fd1d520ab83bd87e06f94dc34dd71
lemonandtheworld <159548263+lemonandtheworld@users.noreply.github.com>
2024-02-09 22:07:46 +0800

    Delete sidebar.php

--- sidebar.php
+++
@@ -1,7 +0,0 @@
-<!-- sidebar.php -->
-<div id="sidebar" class="sidebar">
-    <a href="#" onclick="loadContent('add_product.php')">Add Product</a>
-    <a href="#" onclick="loadContent('v_product.php')">View Products</a>
-    <a href="#" onclick="loadContent('m_sales.php')">Make Sale</a>
-    <a href="#" onclick="loadContent('v_sales.php')">View Sales</a>
-</div>
